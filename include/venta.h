4464ef24a83d0374f361c1719d8a723e2d390315
milv24 <victoriomilvia@gmail.com>
2021-04-02 18:17:31 -0400

    Actualice y cree la rama

--- include/venta.h
+++ include/venta.h
@@ -1,4 +1,11 @@
-/*
-    Ventas: Vender, ver las ventas realizadas, editar pedidos(sudo), eliminar pedidos(sudo), 
-    caja registradora
-*/
+typedef struct IngresodeVentas //Estructura para llenar datos de la facturacion
+{
+    char Nombrecliente[10];
+    char Empleadoenturno[10];
+    char NombreProducto[15];
+    int Cantidad;
+    float Precio;
+    float Total;
+    float TotalaPagar;
+
+}facturas[100]; //vector de 100 para las facturas que quiera hacer

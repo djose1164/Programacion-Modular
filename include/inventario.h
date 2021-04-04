beb227db7df48beeb9b86c566b62d4a5deb1c97e
Jose S. Daniel <djose1164@gmail.com>
2021-04-04 14:26:02 -0400

    Primera definicion del header.

--- include/inventario.h
+++ include/inventario.h
@@ -1,3 +1,51 @@
-/*
-  Inventario: Guardara los productos, Editar productos(sudo), Eliminar producto(sudo), Reporte de inventario
-*/
+/**
+ * @file inventario.h
+ * @author @djose1164
+ * @brief Modulo para la intereccion con la database.
+ * @version 0.1
+ * @date 2021-04-03
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef INVENTARIO_H
+#define INVENTARIO_H
+
+// TODO: Guardar los productos, Editar productos(sudo), Eliminar producto(sudo)
+// TODO: y Reporte de inventario
+/**
+ * @brief Estructura para los productos a guardar en la database.
+ * 
+ */
+struct inventario
+{
+  // Nombre del producto a guardar.
+  const char *product_name;
+  // Precio del producto.
+  const int sell_price;
+  // Cantidad del producto.
+  const int quantity_available;
+};
+
+/**
+ * @brief Crea una tabla si no existe, para guardar los productos en la database.
+ * 
+ * @param product Estructura que contiene los datos a guardar. Mirar los
+ * comentarios hechos en la struct para mas info.
+ * @return true El producto se ha guardado successfully.
+ * @return false No se ha podido guardar.
+ */
+bool save_product(struct inventario *const product);
+
+/**
+ * @brief Edita la informacion de los productos guardados en la database.
+ * 
+ * @param product_id ID del producto a modificar.
+ * @return true 
+ * @return false 
+ */
+bool edit_product(const int product_id);
+
+void report_inventory();
+
+#endif //INVENTARIO_H

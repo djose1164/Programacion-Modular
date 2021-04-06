27ebaec0ffcf4d8bb78d6bf60604f16c8771eb18
djose1164 <djose1164@gmail.com>
2021-04-05 20:04:13 -0400

    Refactorizacion de codigo.
    Ehm, veo que tienes dos while. Si pudieras usar un while y un for; o mejor aun dos for; or un unico for o while (o su otra variante do-while) para todo el bucle. Tambien comentar mas, y agregar mas espacios, y formato de codigo presionando CTRL + SHIFT + P, y escribiendo Formart document.

--- src/login.c
+++ src/login.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>//system
-#include<conio.h>
+//#include<conio.h> No deberias de usar esta lib. Tu codigo dejaria de ser portable.
 #include <string.h> //en esta lib se encuentra la funcion gets y strcmp
 
 //mis credenciales
@@ -24,14 +24,15 @@ pongo mas uno para el caracter NULL*/
         printf("\n\t\t\tINICIO DE SESION\n");
         printf("\t\t\t***************\n");
         printf("\n\tUSUARIO:");
-        gets(usuario);
+        //gets(usuario); Esta funcion esta depreciada. No deberias usarla. En cambio: fgets.
+        fgets(usuario, LONGITUD + 1, stdin);
         printf("\tCLAVE:");
-        while (caracter = getch()) 
+        while (caracter = getchar()) 
         {
-            if(caracter == 13){
+            if(caracter == 13){ // 13? Quieres decir '\n'?
                 clave[i] = '\0';// se guarda el caracter null, me indica el final de una cadena
                 break;
-            }else if (caracter == 8) {
+            }else if (caracter == 8) { // 8? O.o
                 if(i > 0 ){
                        i--;
                 printf("\b \b");//mueve el cursor hacia la izquierda
@@ -45,18 +46,16 @@ pongo mas uno para el caracter NULL*/
             }
             }
         }
-        if (strcmp(ADMIN, usuario) == 0 && strcmp(CLAVEADMIN, clave) == 0)
-        {
-            ingresa = 1;
-          
-        }
+        //if (strcmp(ADMIN, usuario) == 0 && strcmp(CLAVEADMIN, clave) == 0) Esto es muy largo!
+        if(!strcmp(ADMIN, usuario) && !strcmp(CLAVEADMIN, clave)) // Logica negativa. strcmp devuelve 0, negandolo se vuelve 1.
+            ingresa = 1; 
         else
         {
             printf("\n\t Usuario y/o clave incorrectas\n");
             intento++;
             system("pause");
         }
-    } while (intento < 3 && ingresa == 0);
+    } while (intento < 3 && ingresa == 0); 
     if(ingresa == 1){
         printf("\n\t Bienvenido al Sistema \n");
     }else{

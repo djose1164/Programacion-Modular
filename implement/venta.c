30cef8eb49ecba6ac3b094942d024a3013c878c3
Jose S. Daniel <djose1164@gmail.com>
2021-04-06 02:25:31 -0400

    Estilo mejorado.

--- implement/venta.c
+++ implement/venta.c
@@ -1,32 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<conio.h>
-#include"../include/venta.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/venta.h"
 //#include<main.h
 
-void vender ()/*hace la factura y calcula el total de la venta, resta los productos del inventario,*/
+void vender() /*hace la factura y calcula el total de la venta, resta los productos del inventario,*/
 {
-  facturas Facturas [50];
+    facturas Facturas[50];
     system("cls");
 
     printf("\t\t\t\t Modular Programming Ventas \n");
     printf("Rellene con los datos  pedidos\n");
     printf("Nombre cliente \tTotal \n");
-    scanf("%c",&Facturas->Nombrecliente);
+    scanf("%c", &Facturas->Nombrecliente);
     printf("\tAtendido por:");
-    scanf("%c",&Facturas->Empleadoenturno);
+    scanf("%c", &Facturas->Empleadoenturno);
     printf("\tProducto");
-    scanf("%c",&Facturas->NombreProducto);
+    scanf("%c", &Facturas->NombreProducto);
     printf("\tCantidad");
-    scanf("%d",&Facturas->Cantidad);
+    scanf("%d", &Facturas->Cantidad);
     printf("\t Precio");
-    scanf("%f",&Facturas->Precio);
+    scanf("%f", &Facturas->Precio);
     printf("\tTotal ");
-    scanf("%f",&Facturas->Total);
+    scanf("%f", &Facturas->Total);
     printf("\n\n Total a Pagar");
-    scanf("%f",&Facturas->TotalaPagar);
-
+    scanf("%f", &Facturas->TotalaPagar);
 }
 /*void agregar_mas_articulos (){
     int i,j;
--- src/login.c
+++ src/login.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>//system
+#include <stdlib.h> //system
 //#include<conio.h> No deberias de usar esta lib. Tu codigo dejaria de ser portable.
 #include <string.h> //en esta lib se encuentra la funcion gets y strcmp
 
@@ -14,53 +14,61 @@ int main()
     /*variables que van a permitir el nombre de ususario y la contrasena que va a ingresar el usuario que quiere logearse
 pongo mas uno para el caracter NULL*/
     char clave[LONGITUD + 1];
-    int intento = 0;//veces que intenta entrar el usuario
-    int ingresa = 0;// 0 = falso,, 1= verdadero, va a contar cuantas veces ingresa el user
+    int intento = 0; //veces que intenta entrar el usuario
+    int ingresa = 0; // 0 = falso,, 1= verdadero, va a contar cuantas veces ingresa el user
     char caracter;
-    int i = 0;//indice, indica la posicion donde ingresare el caracter 
+    int i = 0; //indice, indica la posicion donde ingresare el caracter
     do
     {
-        system("cls");
+        system("cls||clear");
         printf("\n\t\t\tINICIO DE SESION\n");
         printf("\t\t\t***************\n");
         printf("\n\tUSUARIO:");
         //gets(usuario); Esta funcion esta depreciada. No deberias usarla. En cambio: fgets.
         fgets(usuario, LONGITUD + 1, stdin);
         printf("\tCLAVE:");
-        while (caracter = getchar()) 
+        while (caracter = getchar())
         {
-            if(caracter == 13){ // 13? Quieres decir '\n'?
-                clave[i] = '\0';// se guarda el caracter null, me indica el final de una cadena
+            if (caracter == 13)
+            {                    // 13? Quieres decir '\n'?
+                clave[i] = '\0'; // se guarda el caracter null, me indica el final de una cadena
                 break;
-            }else if (caracter == 8) { // 8? O.o
-                if(i > 0 ){
-                       i--;
-                printf("\b \b");//mueve el cursor hacia la izquierda
-                }
-             
-            }else {
-                if(i < LONGITUD){
-                  printf("*");
-           clave[i] = caracter;
-           i++;
             }
+            else if (caracter == 8)
+            { // 8? O.o
+                if (i > 0)
+                {
+                    i--;
+                    printf("\b \b"); //mueve el cursor hacia la izquierda
+                }
+            }
+            else
+            {
+                if (i < LONGITUD)
+                {
+                    printf("*");
+                    clave[i] = caracter;
+                    i++;
+                }
             }
         }
         //if (strcmp(ADMIN, usuario) == 0 && strcmp(CLAVEADMIN, clave) == 0) Esto es muy largo!
-        if(!strcmp(ADMIN, usuario) && !strcmp(CLAVEADMIN, clave)) // Logica negativa. strcmp devuelve 0, negandolo se vuelve 1.
-            ingresa = 1; 
+        if (!strcmp(ADMIN, usuario) && !strcmp(CLAVEADMIN, clave)) // Logica negativa. strcmp devuelve 0, negandolo se vuelve 1.
+            ingresa = 1;
         else
         {
             printf("\n\t Usuario y/o clave incorrectas\n");
             intento++;
             system("pause");
         }
-    } while (intento < 3 && ingresa == 0); 
-    if(ingresa == 1){
+    } while (intento < 3 && ingresa == 0);
+    if (ingresa == 1)
+    {
         printf("\n\t Bienvenido al Sistema \n");
-    }else{
+    }
+    else
+    {
         printf("\n\t Ha sobrepasado el numero de intentos maximos permitidos \n");
-
     }
 
     return 0;

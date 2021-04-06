afbe29e251b39b298cfe9b77f0e15933e165a3b0
Jose S. Daniel <djose1164@gmail.com>
2021-04-06 17:19:44 -0400

    Update: codigo en general.

--- implement/venta.c
+++ implement/venta.c
@@ -2,56 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/venta.h"
-<<<<<<< HEAD
-#include "../include/iniciologin.h"
+
+// Variables globales.
+facturas Facturas[50];
 
 void vender() /*hace la factura y calcula el total de la venta, resta los productos del inventario,*/
 {
-    typedef struct IngresodeVentas;
-=======
-//#include<main.h
 
-void vender() /*hace la factura y calcula el total de la venta, resta los productos del inventario,*/
-{
->>>>>>> origin/djose1164-patch-1
-    facturas Facturas[50];
     system("cls");
 
     printf("\t\t\t\t Modular Programming Ventas \n");
     printf("Rellene con los datos  pedidos\n");
-<<<<<<< HEAD
-    printf("Nombre cliente \n");
-    fgets("%c", Facturas.); //ingresa nombre del cliente y debe guardarlo para verla mas tarde(no se donde la guardaria)
-    printf("\tAtendido por:");
-    fgets("%c", &Facturas->Empleadoenturno);
-    printf("\tProducto");
-    fgets("%c", &Facturas->NombreProducto);
-    printf("\tCantidad");
-    fgets("%d", &Facturas->Cantidad);
-    printf("\t Precio");
-    fgets("%f", &Facturas->Precio);
-    printf("\tTotal ");
-    fgets("%f", &Facturas->Total);
-    Facturas->Total = Facturas->Precio * Facturas->Cantidad;
-    printf("\n\n Total a Pagar");
-    fgets("%f", &Facturas->TotalaPagar);
-    Facturas->TotalaPagar =
-=======
     printf("Nombre cliente \tTotal \n");
-    scanf("%c", &Facturas->Nombrecliente);
+    scanf("%s", Facturas->Nombrecliente);
     printf("\tAtendido por:");
-    scanf("%c", &Facturas->Empleadoenturno);
+    scanf("%c", Facturas->Empleadoenturno);
     printf("\tProducto");
-    scanf("%c", &Facturas->NombreProducto);
+    strcpy(Facturas->Nombrecliente, "Aca va lo que vas a guradar.");
     printf("\tCantidad");
-    scanf("%d", &Facturas->Cantidad);
+    strcpy(Facturas->NombreProducto, "Aca va lo que vas a guradar.");
     printf("\t Precio");
     scanf("%f", &Facturas->Precio);
     printf("\tTotal ");
     scanf("%f", &Facturas->Total);
     printf("\n\n Total a Pagar");
     scanf("%f", &Facturas->TotalaPagar);
->>>>>>> origin/djose1164-patch-1
 }
 void agregar_mas_articulos()
 { //Da la opcion de agregar mas articulos a la factura con key1
@@ -76,7 +51,7 @@ void agregar_mas_articulos()
     main(); //debe volver a imprimir el menu de inicio sin registrar el user
 }
 void ver_ventas()
-{ //permite ver todas las facturas realizadas de acuerdo al nombre del cliente
+{ //permite ver todas las Facturas realizadas de acuerdo al nombre del cliente
     printf("Usted ha elegido ver ventas \n Ingrese el nombre del cliente: \n");
     fgets("%s"); //necesita buscar el nombre del cliente e imprimir esa factura
 }
@@ -93,8 +68,8 @@ TODO: permite editar cualquier factura, con la clave y usuario del admin
 TODO: Introduciendo el nombre del cliente se elimina automaticamente la factura
 */
 }
-void caja_registradora(facturas)
-{ //Da el total de todas las facturas realizadas
+void caja_registradora(Facturas)
+{ //Da el total de todas las Facturas realizadas
     caja_registradora = Facturas.TotalaPagar
 }
 void salir()
--- include/venta.h
+++ include/venta.h
@@ -1,23 +1,17 @@
+#ifndef VENTA_H
+#define VENTA_H
+
 typedef struct IngresodeVentas //Estructura para llenar datos de la facturacion
 {
-<<<<<<< HEAD
-    char nombre_cliente;
-    char empleado_en_turno;
-    char nombre_producto;
-    int cantidad;
-    float precio;
-    float total;
-    float total_a_Pagar;
-=======
     // La nomenclatura esta mal. Debe ser nombreProducto o nombre_producto.
-    char Nombrecliente;
-    char Empleadoenturno;
-    char NombreProducto;
+    char Nombrecliente[50];
+    char Empleadoenturno[50];
+    char NombreProducto[50];
     int Cantidad;
     float Precio;
     float Total;
     float TotalaPagar;
->>>>>>> origin/djose1164-patch-1
 
-}facturas[100]; //vector de 100 para las facturas que quiera hacer
-facturas Facturas [50];
+} facturas; //vector de 100 para las facturas que quiera hacer
+
+#endif //VENTA_H

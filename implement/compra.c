33505c59bdff167177bbec0bbd499a43b499ca84
Engeers <Engeersrossis@gmail.com>
2021-04-08 17:38:19 -0400

    actualice el header

---
+++ implement/compra.c
@@ -0,0 +1,9 @@
+#include <sqlite3.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "../include/database.h"
+
+
+
+
--- include/compra.h
+++ include/compra.h
@@ -1,4 +1,19 @@
 /*
-    Compras:  Comprar producotos, Crear y comprar productos, Ver compras hechas, Eliminar 
+    Compras:  Comprar productos, Crear y comprar productos, Ver compras hechas, Eliminar 
     y editar compras(sudo)
 */
+#include <stdio.h>
+#include <string.h>
+#include "../include/database.h"
+/* librerias a utilizar */
+
+
+int main(){
+
+    
+
+    getchar();
+    return 0;
+
+
+}

20680bee1f4186f06438d6d3bd0647f497802a2b
milv24 <victoriomilvia@gmail.com>
2021-04-08 23:42:29 -0400

    Update ventas

--- implement/venta.c
+++ implement/venta.c
@@ -4,14 +4,12 @@
 #include "../include/venta.h"
 #include "../include/inventario.h"
 #include "../include/login.h"
-#include<stdbool.h>
-
+#include <stdbool.h>
 
 // Variables globales.
 facturas Facturas[50];
 #define MAX_LETTERS 50
 struct products;
-
 
 /**
  * @brief Rellena la factura y calcula el total por producto y de la venta en general
@@ -21,45 +19,46 @@ struct products;
  * @param total_a_pagar 
  * @return float 
  */
-float vender (float precio, float total, float total_a_pagar)
+float vender(float precio, float total, float total_a_pagar)
 {
     system("cls");
 
     printf("\t\t\t\t Modular Programming Ventas \n"
-    "Rellene con los datos  pedidos\n");
+           "Rellene con los datos  pedidos\n");
     printf("\nNombre cliente:");
-    fgets(Facturas->nombre_cliente,MAX_LETTERS,stdin);
+    fgets(Facturas->nombre_cliente, MAX_LETTERS, stdin);
     printf("\tAtendido por:\n");
-    fgets(Facturas->empleado_en_turno,MAX_LETTERS,stdin);
+    fgets(Facturas->empleado_en_turno, MAX_LETTERS, stdin);
     printf("\n\tProducto");
-    strcpy(Facturas->nombre_producto,);//,"Aca va lo que vas a guardar."
+    strcpy(Facturas->nombre_producto, ); //,"Aca va lo que vas a guardar."
     printf("\tCantidad");
-    strcpy( Facturas->Cantidad,product_name"Aca va lo que vas a guardar.");
-    printf("\t Precio");//Precio no se de donde va a salir, creo que de inventario
-    //TODO quiero hacer que el precio y el total aparezcan solos, dependiendo de la cantidad 
+    strcpy(Facturas->Cantidad,);// TODO La guardare en alguna funcion de inventario que reste los productos
+    printf("\t Precio"); //Precio no se de donde va a salir, creo que de inventario
+    //TODO quiero hacer que el precio y el total aparezcan solos, dependiendo de la cantidad
     //TODO y dependiendo del producto que este ingresando
     scanf("%f", &Facturas->Precio);
     printf("\tTotal ");
     scanf("%f", &Facturas->Total);
-    for (size_t i = 0; i < count; i++)
+
+    /* for (size_t i = 0; i < count; i++)
     {
-        /* code */
-    }
-    
+        
+    }*/
 }
 void agregar_mas_articulos()
 { //Da la opcion de agregar mas articulos a la factura con key1
     int i, j;
     int agregar_articulos = 1;
-    Facturas->Cantidad *= Facturas.//Quiero hacer que el total a pagar sea igual a Cantidad * Precio del producto
+    Facturas->Cantidad *= Facturas. //Quiero hacer que el total a pagar sea igual a Cantidad * Precio del producto
 
-    printf("Desea agregar mas articulos?");
+                          printf("Desea agregar mas articulos?");
     fgets("%d", &agregar_articulos);
-
+    
+    do {
     switch (agregar_articulos)
     {
     case 1:
-        void vender(Facturas); //como puedo llamar la funcion aqui para que se ejecute en este caso
+        float vender(precio, total, total_a_pagar); //como puedo llamar la funcion aqui para que se ejecute en este caso
         break;
 
     case 0:
@@ -67,19 +66,19 @@ void agregar_mas_articulos()
 
         break;
     }
+    }while(agregar_articulos);
     system("cls");
     login_menu(); //debe volver a imprimir el menu de inicio sin registrar el user
 }
 void ver_ventas()
 { //permite ver todas las Facturas realizadas de acuerdo al nombre del cliente
     printf("Usted ha elegido ver ventas \n Ingrese el nombre del cliente: \n");
-    fgets(Facturas->nombre_cliente,MAX_LETTERS,stdin); //necesita buscar el nombre del cliente e imprimir esa factura
-    printf()//TODO ?Como puedo hacer para presentar todas las facturas registradas??
+    fgets(Facturas->nombre_cliente, MAX_LETTERS, stdin); //necesita buscar el nombre del cliente e imprimir esa factura
+    printf()                                             //TODO ?Como puedo hacer para presentar todas las facturas registradas??
 }
 void editar_pedido_sudo()
 {
 
-    
     //TODO: permite editar cualquier factura, con la clave y usuario del admin
 }
 void eliminiar_pedido_sudo()
@@ -91,17 +90,15 @@ TODO: Introduciendo el nombre del cliente se elimina automaticamente la factura
 }
 void caja_registradora()
 { //Da el total de todas las Facturas realizadas
-for (caja_registradora)
-printf("\n\t\t\t******El total de las ventas que hizo ******\n");
-
+    for (caja_registradora)
+        printf("\n\t\t\t******El total de las ventas que hizo ******\n");
 }
 void salir()
 {
-printf("*****Usted salio de Ventas *****");
-Sleep(1);
-system("cls");
-login_menu();
-     
- // TODO: Esta me permite salir del modulo ventas y volver al inicio_login
-    
+    printf("*****Usted salio de Ventas *****");
+    Sleep(1);
+    system("cls");
+    login_menu();
+
+    // TODO: Esta me permite salir del modulo ventas y volver al inicio_login
 }

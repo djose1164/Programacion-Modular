dcc69b6348c09e27457f6b52f8393d91898e1cca
milv24 <victoriomilvia@gmail.com>
2021-04-09 14:06:15 -0400

    Estilo

--- implement/venta.c
+++ implement/venta.c
@@ -22,22 +22,24 @@ struct products;
 float vender(float precio, float total, float total_a_pagar)
 {
     system("cls");
-
     printf("\t\t\t\t Modular Programming Ventas \n"
-           "Rellene con los datos  pedidos\n");
-    printf("\nNombre cliente:");
+           "Rellene con los datos  pedidos\n"
+           "\nNombre cliente:\n"
+           "\tAtendido por:\n"
+           "\n\tProducto"
+           "\tCantidad"
+           "\t Precio"
+           "\tTotal ");
     fgets(Facturas->nombre_cliente, MAX_LETTERS, stdin);
-    printf("\tAtendido por:\n");
     fgets(Facturas->empleado_en_turno, MAX_LETTERS, stdin);
-    printf("\n\tProducto");
-    strcpy(Facturas->nombre_producto, ); //,"Aca va lo que vas a guardar."
-    printf("\tCantidad");
-    strcpy(Facturas->Cantidad,);// TODO La guardare en alguna funcion de inventario que reste los productos
-    printf("\t Precio"); //Precio no se de donde va a salir, creo que de inventario
+    strcpy(Facturas->nombre_producto,"%s" ); //,"Aca va lo que vas a guardar."
+    strcpy(Facturas->Cantidad,"%d"/*Aqui lo que hara es introducir la cantidad de productos*/ );
+    // TODO La guardare en alguna funcion de inventario que reste los productos
+    
+    //Precio no se de donde va a salir, creo que de inventario
     //TODO quiero hacer que el precio y el total aparezcan solos, dependiendo de la cantidad
     //TODO y dependiendo del producto que este ingresando
     scanf("%f", &Facturas->Precio);
-    printf("\tTotal ");
     scanf("%f", &Facturas->Total);
 
     /* for (size_t i = 0; i < count; i++)
@@ -46,27 +48,28 @@ float vender(float precio, float total, float total_a_pagar)
     }*/
 }
 void agregar_mas_articulos()
-{ //Da la opcion de agregar mas articulos a la factura con key1
+{ //Da la opcion de agregar mas articulos a la factura con 1, sino imprime el total a pagar
     int i, j;
     int agregar_articulos = 1;
     Facturas->Cantidad *= Facturas. //Quiero hacer que el total a pagar sea igual a Cantidad * Precio del producto
 
                           printf("Desea agregar mas articulos?");
     fgets("%d", &agregar_articulos);
-    
-    do {
-    switch (agregar_articulos)
+
+    do
     {
-    case 1:
-        float vender(precio, total, total_a_pagar); //como puedo llamar la funcion aqui para que se ejecute en este caso
-        break;
+        switch (agregar_articulos)
+        {
+        case 1:
+            float vender(precio, total, total_a_pagar); //como puedo llamar la funcion aqui para que se ejecute en este caso
+            break;
 
-    case 0:
-        printf("%d", Facturas->TotalaPagar); //Necesito imprimir el total a Pagar, me marca como que no existe esta estruct
+        case 0:
+            printf("%d", Facturas->TotalaPagar); //Necesito imprimir el total a Pagar, me marca como que no existe esta estruct
 
-        break;
-    }
-    }while(agregar_articulos);
+            break;
+        }
+    } while (agregar_articulos);
     system("cls");
     login_menu(); //debe volver a imprimir el menu de inicio sin registrar el user
 }

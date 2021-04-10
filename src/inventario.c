4d2be655af2b40258e37fcb229b178141d9022ff
Jose S. Daniel <djose1164@gmail.com>
2021-04-10 06:29:11 -0400

    Tabla terminada. Y bug critico reparado.

--- src/inventario.c
+++ src/inventario.c
@@ -2,7 +2,7 @@
  * @file inventario.c
  * @author @djose1164
  * @brief 
- * @version 0.1
+ * @version 1.0
  * @date 2021-04-04
  * 
  * @copyright Copyright (c) 2021
@@ -70,19 +70,113 @@ void report_inventory()
 bool edit_product(const unsigned id, const char *new_name,
                   const unsigned new_sellPrice, const unsigned new_availableQuantity)
 {
-    __update__(id, new_name, new_sellPrice, new_availableQuantity);
-    return -1; // Error
+    return __update__(id, new_name, new_sellPrice, new_availableQuantity);
+   
+}
+
+bool ask_to_edit()
+{
+    char _temp[sizeof(int) + sizeof(unsigned)];
+    char product_name[50];
+    unsigned price = 0;
+    unsigned available_quantity = 0;
+    unsigned id = 0;
+    char c;
+
+    fflush(stdout);
+    system("cls||clear");
+
+    printf("\t\t\aHola! Estas actualmente en editar producto!\n\n"
+           "\tEscribe el ID del producto: ");
+    fgets(_temp, sizeof(id), stdin);
+    sscanf(_temp, "%u", &id);
+
+    printf("\t\aIngrese el nuevo nombre del producto: ");
+    fgets(product_name, sizeof(product_name) / sizeof(char), stdin);
+    product_name[strcspn(product_name, "\n")] = 0;
+
+    printf("\t\aIngrese el nuevo precio de venta: ");
+    fgets(_temp, sizeof(price), stdin);
+    sscanf(_temp, "%u", &price);
+
+    printf("\t\aIngrese la nueva cantidad: ");
+    fgets(_temp, sizeof(available_quantity), stdin);
+    sscanf(_temp, "%u", &available_quantity);
+
+    fflush(stdout);
+    system("cls||clear");
+    if (edit_product(id, product_name, price, available_quantity))
+    {
+        printf("\t\aEl producto se ha editado correctamente!\n"
+               "Para editar otro producto escribe 's'; para volver al "
+               "menu Inventario 'n'.\n"
+               "Opcion: ");
+        scanf("%c", &c);
+        getchar();
+        if (c == 'S' || c == 's')
+            return true;
+    }
+
+    return false;
+}
+
+bool ask_to_save()
+{
+    char _temp[sizeof(int) + sizeof(unsigned)];
+    char product_name[50];
+    unsigned price = 0;
+    unsigned available_quantity = 0;
+    unsigned id = 0;
+    do
+    {
+        fflush(stdout);
+        system("cls||clear");
+
+        printf("\t\t\aHola! Estas actualmente en guardar producto!\n\n"
+               "\a\tIngrese el nombre del producto a guardar: ");
+        fgets(product_name, sizeof(product_name) / sizeof(char), stdin);
+        product_name[strcspn(product_name, "\n")] = 0;
+
+        printf("\t\aIngrese el precio de venta: ");
+        //fflush(stdout);
+        fgets(_temp, sizeof(_temp), stdin);
+        sscanf(_temp, "%u", &price);
+
+        printf("\t\aIngrese la cantidad: ");
+        //fflush(stdout);
+        fgets(_temp, sizeof(_temp), stdin);
+        sscanf(_temp, "%u", &available_quantity);
+
+        if (save_product(product_name, price, available_quantity))
+        {
+            fflush(stdout);
+            system("cls||clear");
+            printf("\t\t\aEl producto se ha guardado sastifactoriamente.\n\n"
+                   "\tPara ver todos tus productos guardados vuelve al menu de Inventario.\n");
+        }
+        else
+            fprintf(stderr, "\a\tUps! Esto luce como un bug. Postea un issue!\n");
+
+        printf("\tQuieres anadir algun otro producto?\n"
+               "\tEscribe 's' para si, escribe 'n' para no (volveras al menu de Inventario).\n"
+               "\t\aOpcion: ");
+        if (getchar() == 'S' || getchar() == 's')
+            return true;
+        else
+            return false;
+
+    } while (getchar() == 'S' || getchar() == 's');
 }
 
 bool inventory_menu()
 {
-    char _temp[sizeof(int) + sizeof(unsigned) ];
-    short temp;
-    bool flag = false;
+    char _temp[sizeof(int) + sizeof(unsigned)];
+    short temp = 0;
     char product_name[50];
     unsigned price = 0;
-    unsigned available_quantity = 0; 
+    unsigned available_quantity = 0;
     unsigned id = 0;
+    char c;
 
     fflush(stdout);
     system("cls||clear");
@@ -114,7 +208,7 @@ bool inventory_menu()
 
     do
     {
-        if (flag)
+        if (temp != 0)
             printf("Hey!! Mano q haces? Elige una opcion correcta!\n\n");
         /**Aca empieza el menu. */
         printf("\t\t\aBienvenido al menu de Inventario!\n\n"
@@ -123,63 +217,39 @@ bool inventory_menu()
                "2) Editar producto.\n"
                "3) Reporte de inventario.\n"
                "4) Salir.\n"
-               "\aOpcion: "
-               );
+               "\aOpcion: ");
         fgets(_temp, sizeof(_temp), stdin); // 4 bytes 8 bytes
         sscanf(_temp, "%hd", &temp);
 
         fflush(stdout);
         system("cls||clear");
-
-        flag = true;
     } while (temp <= 0 || temp > 4);
 
     switch (temp)
     {
     case _save_product:
-        
-        fflush(stdout);
-        system("cls||clear");
+        for (; ask_to_save();)
+            ;
+        return inventory_menu();
 
-        printf("\t\t\aHola! Estas actualmente en guardar producto!\n\n"
-               "\a\tIngrese el nombre del producto a guardar: ");
-        fgets(product_name, sizeof(product_name) / sizeof(char), stdin);
-        product_name[strcspn(product_name, "\n")] = 0;
-
-        printf("\t\aIngrese el precio de venta: ");
-        //fflush(stdout);
-        fgets(_temp, sizeof(_temp), stdin);
-        sscanf(_temp, "%u", &price);
-
-        printf("\t\aIngrese la cantidad: ");
-        //fflush(stdout);
-        fgets(_temp, sizeof(_temp), stdin);
-        sscanf(_temp, "%u", &available_quantity);
-
-        save_product(product_name, price, available_quantity);
-        break;
     case _edit_product:
-        printf("\t\t\aHola! Estas actualmente en editar producto!\n\n"
-               "Escribe el ID del producto: ");
-        fgets(_temp, sizeof(price), stdin);
-        sscanf(_temp, "%u", &price); 
+        for (; ask_to_edit();)
+            ;
+        return inventory_menu();
 
-        printf("\t\aIngrese el nuevo nombre del producto: ");
-        fgets(product_name, sizeof(product_name) / sizeof(char), stdin);
-        product_name[strcspn(product_name, "\n")] = 0;
-
-        printf("\t\aIngrese el nuevo precio de venta: ");
-        fgets(_temp, sizeof(price), stdin);
-        sscanf(_temp, "%u", &price);
-
-        printf("\t\aIngrese la nueva cantidad: ");
-        fgets(_temp, sizeof(available_quantity), stdin);
-        sscanf(_temp, "%u", &available_quantity);
-       
-        edit_product(id, product_name, price, available_quantity);
-        break;
     case _report_inventory:
         report_inventory();
+        printf("\n\n\aPresiona 'i' para volver al menu de Inventario; "
+               "'p' para volver al menu principal.\n"
+               "Opcion: ");
+        for (; (c = getchar()) != 'p' || (c = getchar()) != 'P' ||
+               (c = getchar()) != 'i' || (c = getchar()) != 'I';)
+        {
+            if (c == 'p' || c == 'P')
+                return 0; //return login_menu();
+            else if (c == 'i' || c == 'I')
+                return inventory_menu();
+        }
         break;
     case salir:
         return false;

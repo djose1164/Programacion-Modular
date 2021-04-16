981a7ab85254e7ce647ab5fd975796974158df31
robert1090 <avilar516@gmail.com>
2021-04-16 15:28:42 -0400

    Include en Login de Contabilidad.h

--- src/login.c
+++ src/login.c
@@ -12,6 +12,7 @@
 #include "../include/database.h"
 #include "../include/login.h"
 #include "../include/inventario.h"
+#include "../include/contabilidad.h"
 //#include"inventario.h"
 
 // Usuario actual que esta ejecutando el programa.
@@ -92,10 +93,8 @@ int login_menu()
 		getch();
 		break;
 	case CONTABILIDAD:
-		printf("\aUps! En construccion!\n"
-			   "Presione cualquier tecla para finalizar la ejecucion...");
-		getch();
-		break;
+		return contabilidad_menu();
+		
 	case SALIR:
 		fflush(stdout);
 		system("cls||clear");

8187572035e797f62a2b1591113f4e068b65e962
robert1090 <avilar516@gmail.com>
2021-04-16 16:34:17 -0400

    .

--- include/contabilidad.h
+++ include/contabilidad.h
@@ -8,43 +8,17 @@
 #include <stdbool.h>
 /* Prototipos y Declaraciones */
 
-
-/* Fichero a Usar */
-const char reg_contabilidad;
-
-struct cont_reg
-{
-    /* escribe el nombre de la venta en el registro */
-    char reg_nom_ventas[50];
-    /* escribe la cantidad de la venta en el registro */
-    float reg_cant_ventas;
-    /* escribe el ingreso de la venta en el registro */
-    int reg_pre_ventas;
-    /* escribe el nombre de la compra en el registro */
-    char reg_nom_compras[50];
-    /* escribre la cantidad de la compra en el registro */
-    float reg_cant_compras;
-    /* escribe el pago de la compra en el registro */
-    int reg_pre_compras;
-};
+/**
+ * @brief Esta funcion mostrara la lista de los pagos realizadas
+ * 
+ */
+void registro_compras ();
 
 /**
- * @brief Esta funcion se encargara de escribir el registro de contabilidad 
- * en base a las compras y ventas realizadas
+ * @brief Esta funcion mostrara la lista de las ventas realizadas
  * 
- * @param cont_reg 
  */
-void crear_registro (struct cont_reg *cont_reg);
-
-/**
- * @brief Esta funcion mostrara una lista de los ingresos y pagos realizadas
- * 
- * @param registro_compras llevara a cabo una lista de cuentas por pagar 
- * (Es decir, mencionar la cantidad de productos que se agrego y la cantidad que se pago por ello)
- * @param registro_ventas llevara a cabo una lista de las cuentas por cobrar
- * (Es decir, mencionar la cantidad de productos que se retiro y la cantidad de ingresos)
- */
-void registro_contabilidad (int registro_compras, int registro_ventas);
+void registro_ventas ();
 
 /**
  * @brief Esta funcion se encargara de llevar el total de dinero que posee
--- src/contabilidad.c
+++ src/contabilidad.c
@@ -9,57 +9,21 @@
 #include <stdbool.h>
 #include <string.h>
 #include "../include/login.h"
+#include "../include/inventario.h"
+#include "../include/venta.h"
 
-void crear_registro(struct cont_reg *cont_reg) /* Creador del registro */
+void registro_compras() /* Muestra el Registro de Compras realizadas */
 {
-    /* Archivo donde se guardara el registro de contabilidad */
-    const char *reg_contabilidad = {"contabilidad.txt"};
-    FILE *Contabilidad = fopen(reg_contabilidad, "a+");
-
-    if (Contabilidad == NULL) /*Verificador del archivo*/
-    {
-        printf("Error: Problema con el archivo\n");
-        getchar();
-    }
-
-    /* Toma la informacion sobre ventas/compras */
-    //reg_nom_ventas = informacion de ventas;
-    //reg_cant_ventas = informacion de ventas;
-    //reg_pre_ventas = informacion de ventas;
-    //reg_nom_compras = informacion de compras;
-    //reg_cant_compras = informacion de compras;
-    //reg_pre_compras = informacion de compras;
-
-    /* Escritor de ventas en el registro de contabilidad */
-    fprintf(Contabilidad, "%s\n", cont_reg->reg_nom_ventas);
-    fprintf(Contabilidad, "%f\n", cont_reg->reg_cant_ventas);
-    fprintf(Contabilidad, "%d\n", cont_reg->reg_pre_ventas);
-    /* Escritor de compras en el registro de contabilidad */
-    fprintf(Contabilidad, "%s\n", cont_reg->reg_nom_compras);
-    fprintf(Contabilidad, "%f\n", cont_reg->reg_cant_compras);
-    fprintf(Contabilidad, "%d\n", cont_reg->reg_pre_compras);
-
-    fclose(Contabilidad);
+    /* Muestra el Registro Compras */
+    printf("Resgistro de Contabilidad de Compras: \n");
+    return report_inventory();
 }
 
-void registro_contabilidad(int registro_compras, int registro_ventas) /* Muestra el Registro de Ingresos
-                                                                      y Compras realizadas */
+void registro_ventas() /* Muestra el Registro de Ventas realizadas */
 {
-    /* Lector del Registro */
-    const char *reg_contabilidad = {"contabilidad.txt"};
-    FILE *Contabilidad = fopen(reg_contabilidad, "r");
-
-    if (Contabilidad == NULL) /*Verificador del archivo*/
-    {
-        printf("Error: Problema con el archivo\n");
-        getchar();
-    }
-
-    /* Muestra el Registro Completo */
-    printf("Resgistro de Contabilidad: \n"
-           "");
-
-    fclose(Contabilidad);
+    /* Muestra el Registro de Ventas */
+    printf("Resgistro de Contabilidad de Ventas: \n");
+    return print_();
 }
 
 bool contabilidad_menu()
@@ -95,7 +59,7 @@ bool contabilidad_menu()
     switch (temp)
     {
     case _REG:
-        printf("Vuelva mas Tarde");
+        registro_contabilidad;
         break;
 
     case _DT:

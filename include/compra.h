f1875039be4b17837e5bb3a27a8b072f863f6bad
Engeers <Engeersrossis@gmail.com>
2021-04-16 22:14:00 -0400

    datos acualizados n3

--- include/compra.h
+++ include/compra.h
@@ -1,26 +1,18 @@
 #ifndef COMPRA_H
 #define COMPRA_H
-#include <stdbool.h>
+#include <stdlib.h>
 
-enum opciones_compras_menu
+enum opciones_compras_menu//opciones del menu
 {
     COMPRAR_PRODUCTOS = 1,
-    CREAR_PRODUCTOS,
-    ELIMINAR_EDITAR,
+    CREAR_PRODUCTOS ,
+    ELIMINAR_EDITAR ,
     HISTORIAL_DEL_PRODUCTO,
     SALIR_COMPRA
 };
 
-struct Proveedor
-{
-    unsigned id;
-    char producto_nombre[50];
-    unsigned cantidad;
-    unsigned precio;
-    bool full;
-};
-
 /**
+ * 
  * @brief estructuras de comprar producto
  * me servira para organizar la opcion de compras 
  * 
@@ -31,8 +23,11 @@ struct compra
     unsigned precio;
     unsigned cantidad;
     bool comprado;
-    bool eliminado;
 };
+
+
+//variables constantes
+const size_t MAX_COMPRAS;
 
 /*
     Compras:  Comprar productos, Crear y comprar productos, Ver compras hechas, Eliminar 
@@ -52,7 +47,7 @@ bool compras_menu();
  * @return true se ha podido compro el producto.
  * @return false no se pudo comprar el productointente otra vez.
  */
-bool comprar_productos();
+bool comprar_productos(); 
 
 /**
  * @brief esta funcion me permitira crear los productos.  
@@ -84,7 +79,7 @@ bool historial_de_produc();
  * @return true si seleciona te redirecciona al inicio del programa.
  * @return false siendo los opuesto no se podra salir de la opcion de compras.
  */
-void salir();
+bool salir();
 
 /**
  * @brief una vez que se compre algo se tiene que guardar nombre del producto ,cantidad ,precio
@@ -94,15 +89,11 @@ void salir();
  */
 bool compra_historial();
 
-void mostrar_productos_suplidor();
-
-void llenar_productos_suplidor();
-
-/**
- * @brief Devuelve el suplidor.
- * 
- * @return struct Proveedor Una copia de la struct.
- */
-int obtener_suplidor_suma();
-
 #endif //COMPRA_H
+
+
+
+
+
+
+
--- src/compra.c
+++ src/compra.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "../include/compra.h"
-<<<<<<< HEAD
-
-const size_t MAX_COMPRAS = 15;
-    struct compra comprar_producto[MAX_COMPRAS];
-=======
 #include "../include/login.h"
 #include "../include/inventario.h"
 
@@ -89,7 +84,6 @@ bool crear_productos()
 
     return false;
 }
->>>>>>> 6a775b5e45c665dca5afa929d4e0d639c6341a2e
 
 /**
  * @brief mostrar los productos a comprar.
@@ -97,38 +91,6 @@ bool crear_productos()
  * @return true se ha podido compro el producto.
  * @return false no se pudo comprar el productointente otra vez.
  */
-<<<<<<< HEAD
-bool compar_productos(){
-    char nombre[50];
-    unsigned precio;
-    unsigned cantidad;
-
-    clear_screen();
-    for (size_t i = 0; i < MAX_COMPRAS; i++)
-    {
-        printf("Nombre del Producto\tPrecio\tCantidad\n");
-        scanf("%s",&nombre);
-        getchar();
-        scanf("%u",&precio);
-        getchar();
-        scanf("%u",&cantidad);
-    }
-    
-} 
-
-void inicia_producto(struct compra *comprar_producto)
-{
-        for (size_t i = 0; i < MAX_COMPRAS; i++)
-        {
-        comprar_producto[i].comprado = false;
-        }
-}
-
-bool compras_menu()
-{   
-    
-    inicia_producto(&comprar_producto);
-=======
 bool compar_productos()
 {
     char temp[sizeof(unsigned) * 2];
@@ -181,72 +143,19 @@ bool compras_menu()
 {
 
     inicia_producto();
->>>>>>> 6a775b5e45c665dca5afa929d4e0d639c6341a2e
     char _temp[sizeof(short)];
     short temp;
     bool flag = false;
     int time = 2;
-<<<<<<< HEAD
-        /** el sistema de carga del modulo compras*/
-        printf("ingresando al modulo de compras...\n");
-=======
 
     /** el sistema de carga del modulo compras*/
     clear_screen();
     printf("ingresando al modulo de compras...\n");
->>>>>>> 6a775b5e45c665dca5afa929d4e0d639c6341a2e
     system_loading(time);
 
     do
     {
         if (flag)
-<<<<<<< HEAD
-        printf("Hey! Elige una opcion correta\n");
-        /** aqui enpieza la impresion de mi menu*/
-    printf("Bienvenido al menu de compras!\n"
-           "por favor elige una delas siguientes opciones:\n"
-           "1) Comprar productos\n"
-           "2) crear produtos\n"
-           "3) Eliminar/Editar\n"
-           "4) Historial de producto\n"
-           "5) Salir.\n");
-    fgets(_temp, sizeof(_temp), stdin); // \n
-    sscanf(_temp, "%hd",&temp);
-    flag = true;
-        } while (temp <= 0 || temp > 3);
-        switch (temp)
-        {
-        case COMPRAR_PRODUCTOS:
-        compar_productos();
-            break;
-
-        case CREAR_PRODUCTOS:
-        crear_productos();
-            break;
-
-        case ELIMINAR_EDITAR:
-        editar();
-            break;
-
-        case HISTORIAL_DEL_PRODUCTO:
-        historial_de_produc();
-            break;
-
-        case SALIR_COMPRA:
-        salir();
-        return false;
-        default:
-            fprintf(stderr, "Has introducido una funcion incorrecta!"
-                            "Si crees que es un error mande un issue detallando"
-                            "error\n");
-            break;
-        }
-        
-        fflush(stdout);
-        system("cls||clear");
-
-        flag = true;
-=======
             printf("Hey! Elige una opcion correta\n");
         /** aqui enpieza la impresion de mi menu*/
         printf("Bienvenido al menu de compras!\n"
@@ -295,6 +204,5 @@ bool compras_menu()
     system("cls||clear");
 
     flag = true;
->>>>>>> 6a775b5e45c665dca5afa929d4e0d639c6341a2e
     return true;
 }
